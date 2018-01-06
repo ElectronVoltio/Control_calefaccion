de549b5795eba77fa5a0aeb93dd218771158dc58
Electro Voltio <ev@senergy.es>
2018-01-06 08:24:14 +0100

    Delete Error.ino

--- Error.ino
+++
@@ -1,9 +0,0 @@
-void pan_error() {
-  digitalWrite(ledMode, LOW);
-  digitalWrite(releCale, LOW);
-  texto("Fallo lectura", 50, 120, 5, WHITE);
-  texto("sensor DHT!", 80, 160, 5, WHITE);
-  inici = true;
-  i = 0;
-}
-
